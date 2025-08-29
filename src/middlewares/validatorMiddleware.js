c9ec4df6a835d45c8d3dbb810a1e154b53f40905
Tayeb Boulekzazel <ttayeb769@gmail.com>
2025-08-29 20:20:34 +0200

    UPD validator middlware

--- src/middlewares/validatorMiddleware.js
+++ src/middlewares/validatorMiddleware.js
@@ -1,10 +1,14 @@
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 
+exports.runValidation = (req, res, next) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    return next();
+  }
 
-exports.runValidation =(req, res, next) => {
-      const result = validationResult(req);
-      if (result.isEmpty()) {
-        return next();
-      }
-      res.send({ errors: result.array() });
-    }
+  // Send 404 with the validation errors
+  return res.status(404).json({
+    status: "fail",
+    errors: result.array(),
+  });
+};
